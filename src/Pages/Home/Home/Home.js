116902ea2cd23de736d87e6314e1fe477e62f449
siam-26 <siamshahriar390@gmail.com>
2023-05-19 09:45:09 +0600

    treatment section on homepage design

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import CardsInfo from '../CardsInfo/CardsInfo';
 import Services from '../Services/Services';
+import TreatmentSection from '../TreatmentSection/TreatmentSection';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner/>
             <CardsInfo/>
             <Services/>
+            <TreatmentSection/>
         </div>
     );
 };
